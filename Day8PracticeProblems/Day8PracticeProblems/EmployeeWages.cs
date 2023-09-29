92a4f15572538762fd5f588df14e9c949d99a349
BalikiriSudheer <sudheerbalikiri2000@gmail.com>
2023-09-29 21:52:22 +0530

    [Sudheer]Add:EmployeePrsent,Absent

---
+++ Day8PracticeProblems/Day8PracticeProblems/EmployeeWages.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8PracticeProblems
+{
+    internal class EmployeeWages
+    {
+        public void EmpployeeDetails()
+        {
+            int IS_FULL_TIME = 1;
+            Random random=new Random();
+            int empCheck = random.Next(0, 2);
+            if(empCheck==IS_FULL_TIME) {
+                Console.WriteLine("EMPLOYEE IS PRESENT");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
